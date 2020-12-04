cf7648b3b4e61054fe563c275cbb98ef19754d26
ChloeArd <chloe.ardoise@gmail.com>
2020-12-04 10:06:03 +0100

    Exercice 72

--- exo 3/exercice.js
+++ exo 3/exercice.js
@@ -1,3 +1,12 @@
+let premierDiv = document.getElementsByClassName('name-tag');
+let monParagraphe = document.createElement('p');
+
+for (let i = 0; i < premierDiv.length; i++) {
+    monParagraphe[i].innerHTML = "HELLO WORLD";
+    premierDiv.appendChild(monParagraphe);
+}
+
+
 
 
 
